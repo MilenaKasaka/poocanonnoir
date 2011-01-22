88774abebb82acd3a5d37ce443dde8c43aafaa60
ruckpositiv@gmail.com <ruckpositiv@gmail.com@c2238186-79af-eb75-5ca7-4ae61b27dd8b>
2011-01-22 18:06:38 +0000

    copies de fichiers venant de modelecpp mais auxquels j'ai apporté quelques modifications

---
+++ canon_noir/fichiers_gregoire/modif/Case.cpp
@@ -0,0 +1,46 @@
+/**
+* \file Case.cpp
+* \brief Fichier definissant les fonctions de la classe Case
+* \author Sophie Le Corre
+* \author Gregoire Lecourt
+* \version 1.0
+* \date 26/01/2011
+*/
+#include "Case.h"
+
+void Case::prendreTresor()
+{
+}
+
+Case::Case() : type(EAU) , nbTresor(0), altitude(0)
+{
+}
+
+Case::Case(TypeCase t, int n) : type(t) , nbTresor(n)
+{
+}
+
+TypeCase Case::getType()
+{
+	return type;
+}
+
+void Case::setType(TypeCase t)
+{
+	type = t;
+}
+
+int Case::getNbTresor()
+{
+	return nbTresor;
+}
+
+void Case::setNbTresor(int n)
+{
+	nbTresor = n;
+}
+
+void Case::setCoordonnees(pair<int,int> c)
+{
+	coordonnees = c;
+}
---
+++ canon_noir/fichiers_gregoire/modif/Case.h
@@ -0,0 +1,51 @@
+/**
+* \file Case.h
+* \brief Fichier d'en-tete decrivant la classe Case
+* \author Sophie Le Corre
+* \author Gregoire Lecourt
+* \version 1.0
+* \date 26/01/2011
+*/
+#ifndef CASE_H
+#define CASE_H
+
+/// <remarks>
+/// si type = TRESOR alors nbTresor>=0,
+/// sinon nbTresor = 0
+/// </remarks>
+#include <utility>
+#include "enumerations.h"
+
+using namespace std;
+
+/**
+* \class Case
+* \brief Representation d'une case
+* Une case est définie par une position, une altitude, un type et peut contenir un tresor
+*/
+class Case
+{
+	private :
+
+		int nbTresor;
+		TypeCase type;
+		int altitude;
+		pair<int,int> coordonnees;
+
+	public :
+		Case();
+		void prendreTresor();
+		Case(TypeCase t, int n);
+		TypeCase getType();
+		void setType(TypeCase t);
+		int getNbTresor();
+		void setNbTresor(int n);
+		pair<int,int> getCoordonnees() const;
+		int getAltitude() const;
+		void setCoordonnees(pair<int,int> c);
+};
+
+inline pair<int,int> Case::getCoordonnees() const { return coordonnees; }
+
+inline int Case::getAltitude() const { return altitude; }
+#endif
