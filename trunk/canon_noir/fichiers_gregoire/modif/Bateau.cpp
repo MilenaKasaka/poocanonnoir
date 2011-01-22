a8193da48d9f12e2d2932d857c8f7dfa817007ad
ruckpositiv@gmail.com <ruckpositiv@gmail.com@c2238186-79af-eb75-5ca7-4ae61b27dd8b>
2011-01-22 18:20:30 +0000

    Ajout d'une fonction permettant de savoir si le bateau a un trésor ou non

---
+++ trunk/canon_noir/fichiers_gregoire/modif/Bateau.cpp
@@ -0,0 +1,86 @@
+/**
+* \file Bateau.cpp
+* \brief Fichier definissant les fonctions de la classe Bateau
+* \author Sophie Le Corre
+* \author Gregoire Lecourt
+* \version 1.0
+* \date 26/01/2011
+*/
+#include "Bateau.h"
+
+Bateau::Bateau()
+{
+	posX = 0;
+	posY = 0;
+	tresor = false;
+	type = CARAVELLE;
+}
+
+void Bateau::init_port(int port)
+{
+	switch (port)
+	{
+	case 0:
+		posX = 0;
+		posY = 0;
+		break;
+	case 1:
+		posX = LARGEUR-1;
+		posY = 0;
+		break;
+	case 2:
+		posX = LARGEUR-1;
+		posY = HAUTEUR-1;
+		break;
+	case 3:
+		posX = 0;
+		posY = HAUTEUR-1;
+		break;
+	default:
+		posX = 0;
+		posY = 0;
+		break;
+	}
+	//cout << "Port " << port << " : bateau initialise a la position (" << posX << "," << posY << ")" <<endl; 
+}
+
+void Bateau::ajouterTresor()
+{
+	tresor = true;
+}
+
+void Bateau::enleverTresor()
+{
+	tresor = false;
+}
+
+void Bateau::setPosition(int x, int y)
+{
+	posX = x;
+	posY = y;
+}
+
+TypeBateau Bateau::getType()
+{
+	return type;
+}
+
+void Bateau::setType(TypeBateau t)
+{
+	type = t;
+}
+
+void Bateau::retrograderType()
+{
+	if (type == FREGATE) type = RADEAU;
+	else if (type == CARAVELLE) type = FREGATE;
+}
+
+pair<int,int> Bateau::getPosition()
+{
+	return make_pair(posX,posY);
+}
+
+bool Bateau::aTresor() {
+	return tresor;
+}
---
+++ trunk/canon_noir/fichiers_gregoire/modif/Bateau.h
@@ -0,0 +1,46 @@
+/**
+* \file Bateau.h
+* \brief Fichier d'en-tete decrivant la classe Bateau
+* \author Sophie Le Corre
+* \author Gregoire Lecourt
+* \version 1.0
+* \date 26/01/2011
+*/
+#ifndef BATEAU_H
+#define BATEAU_H
+
+#include <iostream>
+#include <utility>
+#include "enumerations.h"
+
+using namespace std;
+
+/**
+* \class Bateau
+* \brief Representation d'un bateau
+* Le bateau est defini par une position, un type et peut contenir un tresor
+*/
+class Bateau
+{
+	private :
+
+		int posX;
+		int posY;
+		bool tresor;
+		TypeBateau type;
+
+	public :
+		Bateau();
+		void init_port(int port);
+		void ajouterTresor();
+		void enleverTresor();
+		void setPosition(int x, int y);
+		TypeBateau getType();
+		void setType(TypeBateau t);
+		void retrograderType();
+		bool aTresor();
+		pair<int,int> getPosition();
+
+}; 
+
+#endif
