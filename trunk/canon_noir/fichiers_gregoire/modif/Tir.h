90d5480452414f4582371386623b0e67f1700a24
ruckpositiv@gmail.com <ruckpositiv@gmail.com@c2238186-79af-eb75-5ca7-4ae61b27dd8b>
2011-01-25 17:51:17 +0000

    C'est pas terminé mais presque

---
+++ trunk/canon_noir/fichiers_gregoire/modif/Tir.h
@@ -0,0 +1,44 @@
+/**
+* \file Tir.h
+* \brief Fichier d'en-tete decrivant la classe Tir
+* \author Sophie Le Corre
+* \author Gregoire Lecourt
+* \version 1.0
+* \date 26/01/2011
+*/
+#ifndef TIR_H
+#define TIR_H
+
+#include <utility>
+#include <math.h>
+
+#include "State.h"
+#include "enumerations.h"
+
+using namespace std;
+
+class Tir : public State
+{
+	private :
+		int angle;
+		int puissance;
+		pair<int,int> posTireur;
+		pair<int,int> direction; // coordonnees de la case visee
+
+		pair<int,int> tirer(); // retourne l'impact du tir
+		pair<int,int> calculerTir(); // calcule la portée du tir
+		
+		double f(unsigned int x);
+		double g(unsigned int y);
+		double z(double d);
+
+	public :
+
+		Tir(Moteur* m);
+		// p = puissance, a = angle (hauteur), c = coordonnée de la case visee
+		void reglerTir(int p, int a, pair<int,int> c);
+		void gerer(); // gere les conséquences du tir sur le jeu
+
+}; 
+
+#endif
